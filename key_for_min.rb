338e1cca114c078ecc8eafa449dad69b34058dcf
Tyler Williams <tyler.williams2996@gmail.com>
2020-06-09 23:31:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+   low_key = nil
+  low_value = nil
+  name_hash.each do |k, v|
+    if low_value == nil|| v < low_value
+      low_key = k
+      low_value = v
+    end
+  end
+  low_key
 end
